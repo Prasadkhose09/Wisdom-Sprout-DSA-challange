484a16f5727e7730a746f4a2fd89df6be633caa7
Prasadkhose09 <Prasadkhose512@gmail.com>
2025-08-28 16:56:31 +0530

    Day Fourteen (Count distinct)

---
+++ DayFourteen.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class DayFourteen {
+    public static int atMostKDistinct(String s, int k) { 
+
+        int n = s.length(), left = 0, result = 0;
+
+
+        Map<Character, Integer> freq = new HashMap<>();
+
+        for (int right = 0; right < n; right++) {
+
+            char c = s.charAt(right);
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+
+            while (freq.size() > k) {
+
+                char leftChar = s.charAt(left);
+                freq.put(leftChar, freq.get(leftChar) - 1);
+
+                if (freq.get(leftChar) == 0) freq.remove(leftChar);
+
+                left++;
+            }
+
+            result += right - left + 1;
+
+        }
+
+        return result;
+    }
+
+
+    
+    public static int countExactlyKDistinct(String s, int k) {
+        if (k == 0) return 0;
+
+
+        return atMostKDistinct(s, k) - atMostKDistinct(s, k - 1);
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+        String s = "pqpqs";
+        int k = 2;
+
+        System.out.println(countExactlyKDistinct(s, k));
+
+    }
+
+    
+}
