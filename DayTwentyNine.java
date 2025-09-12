135f6322767dd0c6f2ddc24ae1d4b5f2545ce236
Prasadkhose09 <Prasadkhose512@gmail.com>
2025-09-12 20:10:06 +0530

    DayTwentyNine Fib series

---
+++ DayTwentyNine.java
@@ -0,0 +1,40 @@
+public class DayTwentyNine {
+    
+    public long fibonacci(int n) {
+
+        if(n == 0) return 0;
+
+
+        if(n == 1) return 1;
+
+
+        long[] dp = new long[n + 1];
+
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+
+        return dp[n];
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+        int n = 5;
+        DayTwentyNine sol = new DayTwentyNine();
+
+        
+        System.out.println(sol.fibonacci(n));
+    }
+}
+
+    
+
